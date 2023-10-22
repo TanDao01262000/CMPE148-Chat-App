d242ab18a4130f0e435aba71fbe4c3be95f30c0e
nhan-magetech <nhantrungha@gmail.com>
2023-10-22 13:58:45 -0700

    adding chat room functionality

--- ChatApp/ChatApp/settings.py
+++ ChatApp/ChatApp/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # apps for this project
+
     'channels',
     'main_app',
 ]
@@ -72,7 +74,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'ChatApp.wsgi.application'
-
+ASGI_APPLICATION = "ChatApp.asgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
--- ChatApp/main_app/urls.py
+++ ChatApp/main_app/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path("", views.index, name="index"),
     path('login/', views.loginView, name='login'),
     path('logout/', views.logoutView, name='logout'),
     path('signup/', views.signup, name='signup'),
+    path("<str:room_name>/", views.room, name="room"),
 ]
